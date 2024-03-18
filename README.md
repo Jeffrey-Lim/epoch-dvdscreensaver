670b9735aa06a2464fbfe96f7f6d61a218f816e3
Jeffrey Lim <J.S.H.Lim@student.tudelft.nl>
2024-03-18 21:50:53 +0100

    Change to Epoch Logo and clean up repository

--- README.md
+++ README.md
@@ -1,38 +1,12 @@
+# Team Epoch DVD Screensaver 📀
 
+The DVD screensaver but it's the [Team Epoch](https://teamepoch.ai/) logo.
 
-## Retro DVD screensaver 📀
+## Features of the bouncing DVD logo: 
 
-After re-watching The Office for the nth time, I decided to recreate this piece of pop culture classic as my first JavaScript project. 
-
-**Short history:**
-
-The bouncing DVD logo was introduced by the international organization DVD Forum and DVD Format/Logo Licensing Corporation, a Tokyo-based international company established in the year 2000 with the purpose of standardizing the DVD format.
-
-While it could be found in any home with a DVD player, it definitely gained notoriety when it became the subject of a popular cold open on the American mockumentary sitcom TV series, *The Office*, episode "Launch Party".
-
-<br/>
-<p align="center">
-  <img src="https://media1.giphy.com/media/02fzO2B9FwQ1GHD5I5/giphy.gif" />
-</p>
-
-### Features of the bouncing DVD logo: 
-
-* Responsive scaling when the window is resized
-* The DVD logo is an SVG asset that:
-	* changes to a random color as it hits the edge of the window
-	* starts blinking as it hits the window corner
-* Custom sound effects for hitting the edge of the window and when hitting the window corner
-
-
-<p align="center">
-  <img src="https://media.giphy.com/media/d1s0X5nBJgfRq3m09z/giphy.gif" />
-</p>
-
-
+* Responsive scaling when the window is resized. 
+* The Team Epoch logo is an SVG asset that changes to a random color as it hits the edge of the window.
 
 ## Live demo
 
-You can view a live demo of this project [here](https://raw.githack.com/boglarkasebestyen/dvdscreensaver/master/index.html).
-
-
-
+You can view a live demo of this project [here](https://jeffrey-lim.github.io/epoch-dvdscreensaver/).
--- dvd.js
+++ dvd.js
@@ -1,188 +1,142 @@
-
-//SOUNDS
-var soundX = new Audio("bleep.aif");
-var soundY = new Audio("bleep.aif");
-var soundCorner = new Audio("shortsaxish.aif");
-
-var cornerAnimationTimer;
-var frameTimerInterval = 5;
+const frameTimerInterval = 5;
 
 function getPixelValue(stringValue) {
-  return parseInt(stringValue.replace("px", ""));
+    return parseInt(stringValue.replace("px", ""));
 }
 
 //COLOR FOR DVD LOGO
 function getRandomColor() {
-  var characters = "123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 3; i++) {
-    var randomIndex = Math.floor(Math.random() * 15);
-    color += characters[randomIndex] + characters[randomIndex];
-  }
-  return color;
+    const characters = "123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * 15);
+        color += characters[randomIndex] + characters[randomIndex];
+    }
+    return color;
 }
 
 //CHANGE COLOR OF DVD LOGO SVG
-function changeCubeColor(animationBox, sound, cornerDetection) {
-    document.getElementById("dvdLogo").setAttribute("fill", getRandomColor());
-    if (cornerDetection) sound.play(); 
+function changeCubeColor() {
+    document.getElementById("logo").setAttribute("fill", getRandomColor());
 }
-
-function onCornerHit() {
-  clearInterval(cornerAnimationTimer);
-  cornerAnimationTimer = setInterval(vibrateRed, 60);
-  var animationCount = 0;
-  var newColor = getRandomColor();
-
-  function vibrateRed() {
-      if (animationCount % 2 == 0) {
-        document.getElementById("dvdLogo").setAttribute("fill", newColor);
-      } else {
-        document.getElementById("dvdLogo").setAttribute("fill", "#ff0000");
-      }
-
-      if (++animationCount > 26) {
-        clearInterval(cornerAnimationTimer);
-      }
-  }
-
-  soundX.pause();
-  soundY.pause();
-  soundCorner.play();
-}
-
 
 function almostEqual(a, b) {
-  return Math.abs(a - b) <= 5;
+    return Math.abs(a - b) <= 5;
 }
 
-
 function detectCorner(newX, newY, maxX, maxY, w, h) {
+    const newXwidth = newX + w;
+    const newYheight = newY + h;
 
-  var newXwidth = newX + w;
-  var newYheight = newY + h;
+    const topLeft = almostEqual(newX, 0) && almostEqual(newY, 0);
+    const topRight = almostEqual(newXwidth, maxX) && almostEqual(newY, 0);
+    const bottomRight = almostEqual(newXwidth, maxX) && almostEqual(newYheight, maxY);
+    const bottomLeft = almostEqual(newX, 0) && almostEqual(newYheight, maxY);
 
-  var topLeft = almostEqual(newX,0) && almostEqual(newY, 0);
-  var topRight = almostEqual(newXwidth, maxX) && almostEqual(newY, 0);
-  var bottomRight = almostEqual(newXwidth, maxX) && almostEqual(newYheight, maxY);
-  var bottomLeft = almostEqual(newX, 0) && almostEqual(newYheight, maxY);
-
-  if (topLeft || topRight || bottomRight || bottomLeft) {
-    onCornerHit();
-    return true;
-  }
-
-  return false;
+    return topLeft || topRight || bottomRight || bottomLeft;
 }
 
 
 //ANIMATIONBOX'S MOVEMENT
 function movingBox() {
-  var animationBox = $("#animationBox");
-  var timer = setInterval(frame, frameTimerInterval); 
+    const animationBox = $("#animationBox");
+    let timer = setInterval(frame, frameTimerInterval);
 
-  var oldX = getPixelValue(animationBox.css("left"));
-  var oldY = getPixelValue(animationBox.css("top"));
+    let oldX = getPixelValue(animationBox.css("left"));
+    let oldY = getPixelValue(animationBox.css("top"));
 
-  var directionX = 1.0;
-  var directionY = 1.0;
+    let directionX = 1.0;
+    let directionY = 1.0;
 
-  var cornerDetection = true;
-  var framesSinceCorner = 0;
+    let cornerDetection = true;
+    let framesSinceCorner = 0;
 
-  var maxX = 0;
-  var maxY = 0;
+    let maxX = 0;
+    let maxY = 0;
 
     function frame() {
+        maxX = $(document).width() - animationBox.width();
+        maxY = $(document).height() - animationBox.height();
 
-      maxX = $(document).width() - animationBox.width();
-      maxY = $(document).height() - animationBox.height();
+        let newX = oldX;
+        let newY = oldY;
 
-      var newX = oldX;
-      var newY = oldY;
-
-      if (oldX <= 0 || oldX >= maxX) { 
-        directionX = directionX * -1;
-        changeCubeColor(animationBox, soundX, cornerDetection);
-      } 
-
-      newX = newX + 2 * directionX;
-
-      if (oldY <= 0 || oldY >= maxY) {
-        directionY = directionY * -1;
-        changeCubeColor(animationBox, soundY, cornerDetection);
-      } 
-
-      newY = newY + 2 * directionY;
-
-      oldX = newX;
-      oldY = newY;
-
-      animationBox.css("left", newX + "px"); 
-      animationBox.css("top", newY + "px"); 
-      
-      if (cornerDetection && detectCorner(newX, newY, $(document).width(), $(document).height(), animationBox.width(), animationBox.height())) {
-          cornerDetection = false;
-      } else if (++framesSinceCorner > 50) {
-          cornerDetection = true;
-          framesSinceCorner = 0;
-      }
-    }
-
-    var mouseDown = false;
-    var resizeTimer;
-    
-    function detectResizeEnd() {
-      
-      var newMaxX = Math.floor($(document).width() - animationBox.width());
-      var newMaxY = Math.floor($(document).width() - animationBox.height());
-
-      if ((newMaxX == Math.floor(maxX)) && (newMaxY = Math.floor(maxY))) {
-        clearInterval(resizeTimer);
-        if (timer === null) {
-          timer = setInterval(frame, frameTimerInterval);
+        if (oldX <= 0 || oldX >= maxX) {
+            directionX = directionX * -1;
+            changeCubeColor();
         }
-      }
-    }
 
-    $(window).resize(
-      function () {
-        clearInterval(resizeTimer);
-        resizeTimer = setInterval(detectResizeEnd, 10);
+        newX = newX + 2 * directionX;
 
-        clearInterval(timer);
-        timer = null;
+        if (oldY <= 0 || oldY >= maxY) {
+            directionY = directionY * -1;
+            changeCubeColor();
+        }
 
-        oldX = getPixelValue(animationBox.css("left"));
-        oldY = getPixelValue(animationBox.css("top"));
-
-        var newMaxX = $(document).width() - animationBox.width();
-        var newMaxY = $(document).height() - animationBox.height();
-
-        var differenceX = maxX - newMaxX;
-        var differenceY = maxY - newMaxY;
-
-        var newX = oldX - differenceX;
-        var newY = oldY - differenceY;
-
-        newX = (newX > 0) ? newX : 1;
-        newY = (newY > 0) ? newY : 1;
-
-        animationBox.css("left", newX + "px"); 
-        animationBox.css("top", newY + "px"); 
-
-        maxX = newMaxX;
-        maxY = newMaxY;
+        newY = newY + 2 * directionY;
 
         oldX = newX;
         oldY = newY;
-      
-    }); // resize
-  } // movingBox
-    
 
-$(document).ready(function() {
-  document.getElementById("dvdLogo").setAttribute("fill", getRandomColor());
-  movingBox();
+        animationBox.css("left", newX + "px");
+        animationBox.css("top", newY + "px");
+
+        if (cornerDetection && detectCorner(newX, newY, $(document).width(), $(document).height(), animationBox.width(), animationBox.height())) {
+            cornerDetection = false;
+        } else if (++framesSinceCorner > 50) {
+            cornerDetection = true;
+            framesSinceCorner = 0;
+        }
+    }
+
+    let resizeTimer;
+
+    function detectResizeEnd() {
+        const newMaxX = Math.floor($(document).width() - animationBox.width());
+
+        if (newMaxX === Math.floor(maxX)) {
+            clearInterval(resizeTimer);
+            if (timer === null) {
+                timer = setInterval(frame, frameTimerInterval);
+            }
+        }
+    }
+
+    $(window).resize(
+        function () {
+            clearInterval(resizeTimer);
+            resizeTimer = setInterval(detectResizeEnd, 10);
+
+            clearInterval(timer);
+            timer = null;
+
+            oldX = getPixelValue(animationBox.css("left"));
+            oldY = getPixelValue(animationBox.css("top"));
+
+            const newMaxX = $(document).width() - animationBox.width();
+            const newMaxY = $(document).height() - animationBox.height();
+
+            const differenceX = maxX - newMaxX;
+            const differenceY = maxY - newMaxY;
+
+            let newX = oldX - differenceX;
+            let newY = oldY - differenceY;
+
+            newX = (newX > 0) ? newX : 1;
+            newY = (newY > 0) ? newY : 1;
+
+            animationBox.css("left", newX + "px");
+            animationBox.css("top", newY + "px");
+
+            maxX = newMaxX;
+            maxY = newMaxY;
+
+            oldX = newX;
+            oldY = newY;
+        }); // resize
+} // movingBox
+
+$(document).ready(function () {
+    document.getElementById("logo").setAttribute("fill", getRandomColor());
+    movingBox();
 });
-
